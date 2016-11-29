8929224415bf6256c17b01c3aefc1fbeba986fc1
Isaac <r.sato7690@me.com>
2016-11-29 22:05:01 +0900

    Fix drawer style

--- app/main.py
+++ app/main.py
@@ -22,17 +22,9 @@ def index_boardList():
     html = urlopen(url)
     soup = BeautifulSoup(html, "html.parser")
     small = soup.find("small")
-    """
-    category = {}
-    for b in small.find_all('b'):
-        category[b.text] = []
-        for a in b.find_next_siblings():
-            if a.name == "b": break
-            category[b.text].append({"name": a.text, "href": a.get("href")})
-    return jsonify(category)
-    """
     json = []
     count = 0
+
     for b in small.find_all('b'):
         json.append({"category": b.text, "boards": []})
         for a in b.find_next_siblings():
