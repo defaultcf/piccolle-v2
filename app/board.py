01aeb4a6fe520396af62e4582ed7c8b0de81853e
Isaac <r.sato7690@me.com>
2016-11-30 14:53:19 +0900

    Modify for each 2ch

--- app/board.py
+++ app/board.py
@@ -13,11 +13,10 @@ class Board():
 
         html = urlopen(url)
         soup = BeautifulSoup(html, "html.parser")
-        small = soup.find("small")
         json = []
         count = 0
 
-        for b in small.find_all('b'):
+        for b in soup.find_all('b'):
             json.append({"category": b.text, "boards": []})
             for a in b.find_next_siblings():
                 if a.name == 'b': break
